5d59139ee0fe8213df33e0ac856be71b0d7796d2
Christian Falck <christian.falck@gmail.com>
2024-02-25 22:20:03 +0100

    removed duplicates

--- 2018/2018day1.py
+++ 2018/2018day1.py
@@ -1,11 +1,18 @@
-def parse_file(file_path):
-    start = 0
-    with open(file_path, 'r') as file:
-        for line in file:
-            # Convert the line to an integer and add it to start
-            start += int(line.strip())  # Use strip() to remove leading/trailing whitespaces
+import itertools
 
-    print(start)
+# part 1
+start = 0
+numbers = [int(x) for x in open("C:\\Users\\falke\\Documents\\Projects\\adventchallenge\\2018\\2018day1.txt").readlines()]
+for number in numbers:
+    start += number
+print(start)
 
-file_path = 'C:\\Users\\falke\\Documents\\Projects\\2018\\2018day1.txt'
-parse_file(file_path)
+# part 2
+start = 0
+seen = set()
+for number in itertools.cycle(numbers):
+    start += number
+    if start in seen:
+        print(start)
+        break
+    seen.add(start)
